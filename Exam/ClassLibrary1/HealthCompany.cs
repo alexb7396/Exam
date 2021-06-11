a708fb460c88f7941894c623758ccb9fa4281bd8
Andrea Moretto <4850210@studenti.unige.it>
2021-06-11 11:26:12 +0200

    .

--- Exam/ClassLibrary1/HealthCompany.cs
+++ Exam/ClassLibrary1/HealthCompany.cs
@@ -14,9 +14,33 @@ namespace ContactManagement
             PositiveCitizens.Add(positiveCitizen);
             TrakedCitizens.Add(positiveCitizen);
         }
+        public void AddTrakedCitizen(Citizen citizen)
+        {
+            TrakedCitizens.Add(citizen);
+        }
         public void SwitchCitizenState(Citizen citizen,bool ToPositive)
         {
+            if (!TrakedCitizens.Contains(citizen))
+            {
+                throw new ApplicationException("This citizen is not traked");
+            }
+            else if(TrakedCitizens.Contains(citizen)&&!ToPositive)
+            {
+                throw new ApplicationException("This citizen is already non-positive");
+            }
+            else if (ToPositive && PositiveCitizens.Contains(citizen))
+            {
+                throw new ApplicationException("This citizen is already positive");
+            }
 
+            if(ToPositive)
+            {
+                PositiveCitizens.Add(citizen);
+            }
+            else
+            {
+                PositiveCitizens.Remove(citizen);
+            }
         }
     }
 }
