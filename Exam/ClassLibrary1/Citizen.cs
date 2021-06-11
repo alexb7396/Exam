46d5427df51ec1ad1d8e583280cc79116e58a84a
Alessandro Balestra <alexb7396@gmail.com>
2021-06-11 11:41:25 +0200

    x

--- Exam/ClassLibrary1/Citizen.cs
+++ Exam/ClassLibrary1/Citizen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace ContactManagement
 {
@@ -8,6 +9,7 @@ namespace ContactManagement
     {
         private double ID { get; set; }
         private bool State { get; set; }
+        private bool contactWithPositive;
         private List<Citizen> contact = new List<Citizen>();
         public bool GetState()
         {
@@ -16,6 +18,14 @@ namespace ContactManagement
         public void ChangeState()
         {
             State = !State;
+        }
+        public void ManageContact(Citizen citizien,TimeSpan contactTime)
+        {
+            if(contactTime.TotalMinutes>= 15 && !contact.Contains(citizien))
+            {
+                contactWithPositive = citizien.GetState();
+                contact.Add(citizien);
+            }
         }
     }
 }
