f092247b3c87f21f78f3911fd9d3286870a88c80
Alessandro Balestra <alexb7396@gmail.com>
2021-06-11 11:25:43 +0200

    x

--- Exam/ClassLibrary1/Citizen.cs
+++ Exam/ClassLibrary1/Citizen.cs
@@ -6,6 +6,16 @@ namespace ContactManagement
 {
     public class Citizen : Person, IHaveCovid 
     {
-
+        private double ID { get; set; }
+        private bool State { get; set; }
+        private List<Citizen> contact = new List<Citizen>();
+        public bool GetState()
+        {
+            return State;
+        }
+        public void ChangeState()
+        {
+            State = !State;
+        }
     }
 }
